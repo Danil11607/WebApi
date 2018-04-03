3e697756627fb1c8ab9796e7d566f0b1df31f85d
Danil11607 <DMButin@stud.kpfu.ru>
2018-04-04 00:25:31 +0300

    entity, db

---
+++ WebApi/WebApi/Data/MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+        }
+
+        public MyDbContext(DbContextOptions opts) : base(opts) { }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
